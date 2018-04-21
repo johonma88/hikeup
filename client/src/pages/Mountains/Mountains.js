a70ba7b7d70eff25c86272be2981b3eb95119a32
Anthony Kehn <anthony@kehn.net>
2018-04-21 10:06:05 -0600

    ModalAnthony

--- client/src/pages/Mountains/Mountains.js
+++ client/src/pages/Mountains/Mountains.js
@@ -7,6 +7,7 @@ import { List, ListItem } from "../../components/List";
 import "../../components/DropdownBtn/DropdownBtn.css";
 import SideMenu from  "../../components/SideMenu";
 import Modal from  "../../components/Modal/Modal";
+import ThumbnailCustom from "../../components/ThumbnailCustom";
 
 
 class Mountains extends Component {
@@ -19,7 +20,8 @@ class Mountains extends Component {
     lat: "",
     lon: "",
     weather: "",
-    show: false
+    show: false,
+    mountain: {}
   };
 
 
@@ -34,6 +36,7 @@ class Mountains extends Component {
       )
       .catch(err => console.log(err));
   };
+  
 
   deleteMtInfo = id => {
     API.deleteMtInfo(id)
@@ -83,12 +86,6 @@ class Mountains extends Component {
               <h1>14ers List</h1>
             </Jumbotron>
 
-            <input type="button"
-                  onClick={this.showModal}
-                   value="Show Modal" />
-
-            
-
             {this.state.mtsinfo.length ? (
               <List>
                 {this.state.mtsinfo.map(mtinfo => (
@@ -98,13 +95,31 @@ class Mountains extends Component {
                         Mountain Range: {mtinfo.mtranges} <br /> 
                         14ner: {mtinfo.fourteeners} <br />
                       </strong>
-                      <Link to={"/mtsinfo/" + mtinfo._id}>More Details
-                      <Modal 
+                      <input type="button"
+                  onClick={this.showModal}
+                   value="Show Modal" />
+                      {/* <Link to={"/mtsinfo/" + mtinfo._id}>More Details */}
+                      <Modal
+                        key={mtinfo._id}
                         onClose={this.showModal}
                         show={this.state.show}>
-                        This message is from Modal!
+                      <div class="thumbnail">
+               <ThumbnailCustom key={this.state.mountain._id}>
+                <img src={mtinfo.picture} alt=""/>
+            </ThumbnailCustom>
+            <hr/>
+                <div class="caption">
+                <h3>Details for {mtinfo.mtranges} </h3>
+                    <p>Mountain Range: {mtinfo.mtranges}</p>
+                    <p>Fourteener: {mtinfo.fourteeners}</p>
+                    <p>Elevation: {mtinfo.elevation}</p>
+                    <p>Latitude: {mtinfo.lat} Longitude: {this.state.mountain.lon}</p>
+                    <p>Weather:  <a href={mtinfo.weather}  target="_blank">Click here to check the weather </a></p>
+                    {/* <Link to="/mtsinfo">← Back to Mountain List</Link> */}
+              </div>
+              </div>
                       </Modal>
-                    </Link>
+                    {/* </Link> */}
                   </ListItem>
                 ))}
               </List>
