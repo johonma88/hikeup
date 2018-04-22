a8327aea1d18eb36472dd7286babee247004d55d
Johanna Honma <johonma@hotmail.com>
2018-04-21 21:55:13 -0600

    last changes

--- client/src/pages/NoMatch/NoMatch.js
+++ client/src/pages/NoMatch/NoMatch.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { Col, Row, Container } from "../../components/Grid";
 import Jumbotron from "../../components/Jumbotron";
+import ThumbnailCustom from "../../components/ThumbnailCustom";
 
 const NoMatch = () => (
   <Container fluid>
-    <Row>
-      <Col size="md-12">
+  
         <Jumbotron>
           <h1>404 Page Not Found</h1>
-          <h1>
+          {/* <h1>
             <span role="img" aria-label="Face With Rolling Eyes Emoji">
               🙄
             </span>
-          </h1>
+          </h1> */}
+           
         </Jumbotron>
-      </Col>
-    </Row>
+        <ThumbnailCustom >
+                    <img src="https://media.giphy.com/media/26tPhNJomQlAbEBy0/giphy.gif" alt=""/>
+               </ThumbnailCustom>
+  
   </Container>
 );
 
