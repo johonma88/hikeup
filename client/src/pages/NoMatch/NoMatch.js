20c5ec3cd18109a43f73a4260761934be012ed05
Anthony Kehn <anthony@kehn.net>
2018-04-25 20:14:37 -0600

    404 & Footer Fix

--- client/src/pages/NoMatch/NoMatch.js
+++ client/src/pages/NoMatch/NoMatch.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "../../components/Grid";
 import Jumbotron from "../../components/Jumbotron";
 import ThumbnailCustom from "../../components/ThumbnailCustom";
+import "./NoMatch.css";
 
 const NoMatch = () => (
   <Container fluid>
@@ -11,10 +12,11 @@ const NoMatch = () => (
         </Jumbotron>
 
         <ThumbnailCustom >
-                    <img src="https://media.giphy.com/media/26tPhNJomQlAbEBy0/giphy.gif" alt="Slip and Fall"/>
+                    <img id="noMatch" src="https://media.giphy.com/media/26tPhNJomQlAbEBy0/giphy.gif" alt="Slip and Fall"/>
         </ThumbnailCustom>
   
   </Container>
 );
 
 export default NoMatch;
+
