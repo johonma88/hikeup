8a371b7c8a189893e3006d71e54a488539ff54d4
Anthony Kehn <anthony@kehn.net>
2018-04-26 20:22:19 -0600

    Anthony

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -7,11 +7,7 @@ const Footer = () =>
 <footer class="page-footer font-small blue pt-4 mt-4">
 
 <div class="footer-copyright py-3 text-center padStyle">
-<<<<<<< HEAD
-<span id="footer"> © 2018 Copyright:</span> <a href="/"> CodeAholics </a>
-=======
     <span id="footer"> © 2018 Copyright:</span> <a href="/"> CodeAholics </a>
->>>>>>> 099d6766d910ed15799d28db9ab20d6256a99243
 </div>
 
 </footer>
